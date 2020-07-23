19b33d3ca3c6c964a99948fb9c2c2fcf10b93ce5
Ali Eldamiry <alieldamiry@gmail.com>
2020-07-23 14:14:29 +0200

    Hosting

--- src/containers/Home/Home.js
+++ src/containers/Home/Home.js
@@ -9,7 +9,7 @@ const Home = () => {
                 <img className={classes.HeaderImage} alt="" src={imageSource} />
                 <div className={classes.HeaderText}>
                     <h6>THIS WEEK ONLY</h6>
-                    <h1>Crazy Sale</h1>
+                    <h1>Big Sale</h1>
                     <h2><span style={{ color: '#007cc3' }}>50% Off</span></h2>
                     <button className={classes.HeaderButton}><strong>SHOP NOW</strong></button>
                 </div>
